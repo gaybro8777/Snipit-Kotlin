25c696b02c6f8cb133aa85d578017e16397347ba
oday.maleh <oday.m@mcsaatchi.me>
2017-10-19 11:49:18 +0300

    Latest

--- app/src/main/kotlin/com/om/snipit/BooksActivity.kt
+++ app/src/main/kotlin/com/om/snipit/BooksActivity.kt
@@ -1,15 +1,26 @@
 package com.om.snipit
 
 import android.os.Bundle
+import com.om.snipit.models.Book
+import timber.log.Timber
 
-class BooksActivity : BaseActivity() {
+class BooksActivity : BaseActivity(), BooksActivityPresenter.BooksActivityView {
 
-//  private val presenter = BooksActivityPresenter()
+  private val presenter = BooksActivityPresenter()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_books)
 
+    presenter.insertBook(
+        Book(title = "Book 1", author = "Author 1", image_path = "testing 123",
+            date_added = "24 December, 2017", color_code = 2, list_order = 2)
+        , dbHelper)
 
+    presenter.getBooks(this, dbHelper)
+  }
+
+  override fun displayBooks(books: ArrayList<Book>) {
+    Timber.d("List of all books is : ${books.size}")
   }
 }
