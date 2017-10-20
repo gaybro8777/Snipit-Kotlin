9f18d3ab3c8947760f343b6ff09a70eb19dce0dc
oday.maleh <oday.m@mcsaatchi.me>
2017-10-20 17:20:03 +0300

    Beginning implementation for Books Activity

--- app/src/main/kotlin/com/om/snipit/books/BooksActivity.kt
+++ app/src/main/kotlin/com/om/snipit/books/BooksActivity.kt
@@ -1,10 +1,9 @@
 package com.om.snipit.books
 
 import android.os.Bundle
-import com.om.snipit.base.BaseActivity
 import com.om.snipit.R
+import com.om.snipit.base.BaseActivity
 import com.om.snipit.models.Book
-import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : BaseActivity(), BooksActivityPresenter.BooksActivityView {
 
@@ -23,6 +22,6 @@ class BooksActivity : BaseActivity(), BooksActivityPresenter.BooksActivityView {
   }
 
   override fun displayBooks(books: ArrayList<Book>) {
-    textView.text = "List of all books is : ${books.size}"
+
   }
 }
