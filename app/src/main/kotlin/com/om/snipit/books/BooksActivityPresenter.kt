be6b228a91e863000cca9ef5df4125ea8a928299
oday.maleh <oday.m@mcsaatchi.me>
2017-11-10 17:28:33 +0200

    reverted

--- app/src/main/kotlin/com/om/snipit/books/BooksActivityPresenter.kt
+++ app/src/main/kotlin/com/om/snipit/books/BooksActivityPresenter.kt
@@ -5,11 +5,19 @@ import com.om.snipit.database.entities.Book
 import kotlinx.coroutines.experimental.async
 
 class BooksActivityPresenter {
-  fun getBooks(view: BooksActivityView, dbHelper: AppDatabase) = async {
-    val books = dbHelper.bookDao().all
-
-    view.displayBooks(books)
+  fun getBooks(view: BooksActivityView, dbHelper: AppDatabase)
+  {
+    view.displayBooks(mutableListOf(Book(title = "Book 1", author = "Author 1", imagePath = "testing 123",
+            dateAdded = "24 December, 2017", colorCode = 2, listOrder = 2), Book(title = "Book 1", author = "Author 1", imagePath = "testing 123",
+        dateAdded = "24 December, 2017", colorCode = 2, listOrder = 2), Book(title = "Book 1", author = "Author 1", imagePath = "testing 123",
+        dateAdded = "24 December, 2017", colorCode = 2, listOrder = 2), Book(title = "Book 1", author = "Author 1", imagePath = "testing 123",
+        dateAdded = "24 December, 2017", colorCode = 2, listOrder = 2)))
   }
+//      = async {
+//    val books = dbHelper.bookDao().all
+//
+//    view.displayBooks(books)
+//  }
 
   fun insertBook(book: Book, dbHelper: AppDatabase) = async {
     dbHelper.bookDao().insert(book)
